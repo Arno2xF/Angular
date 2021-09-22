524e96414d6770f64f11a68a75b8b0700feb4ebc
Arnaud <aleclaire@beckman.com>
2021-09-22 10:13:57 +0200

    dev: mapping

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Routes/home/home.component';
+import { LegalComponent } from './Routes/legal/legal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'legal',
+    component: LegalComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
