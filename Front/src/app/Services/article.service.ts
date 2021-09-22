0aa320fc9ff58d8b8d36f064c3405f21179d9456
Arnaud <aleclaire@beckman.com>
2021-09-22 17:07:26 +0200

    dev: localstorage

--- Front/src/app/Services/article.service.ts
+++ Front/src/app/Services/article.service.ts
@@ -5,24 +5,40 @@ import { Article } from '../interfaces/article';
   providedIn: 'root'
 })
 export class ArticleService {
-  public articles: Article[] = [
-    {
-      name: 'Tournevis', price: 2.99, qty: 120
-    },
-    {
-      name: 'Marteau', price: 7.90, qty: 45
-    },
-    {
-      name: 'Perceuse', price: 25.99, qty: 180
-    },
-    {
-      name: 'Pelle', price: 15.00, qty: 7
-    }
-  ];
+  public articles!: Article[];
 
-  constructor() { }
+  constructor() { 
+    this.load();
+  }
 
   public addArticle(article: Article) {
     this.articles.push(article);
+    this.save();
+  }
+
+  public load() {
+    const str = localStorage.getItem("articles");
+    if (!str) {
+      this.articles = [
+        {
+          name: 'Tournevis', price: 2.99, qty: 120
+        },
+        {
+          name: 'Marteau', price: 7.90, qty: 45
+        },
+        {
+          name: 'Perceuse', price: 25.99, qty: 180
+        },
+        {
+          name: 'Pelle', price: 15.00, qty: 7
+        }
+      ];
+      return;
+    }
+    this.articles = JSON.parse(str);
+  }
+
+  public save() {
+    localStorage.setItem("articles", JSON.stringify(this.articles));
   }
 }
