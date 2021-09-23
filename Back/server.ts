7c54852a77758247f6e08dbc33782df1ada8a8ab
Arnaud <aleclaire@beckman.com>
2021-09-23 15:09:37 +0200

    dev: implement retour article back

--- Back/server.ts
+++ Back/server.ts
@@ -3,16 +3,43 @@ console.log("Start Server");
 import express from "Express";
 import serveIndex from "serve-index";
 import cors from "cors";
+import { Article } from "../Front/src/app/interfaces/article";
 
 const app = express();
 const port = 3000;
 const dir = "../front/dist/front";
+const articles: Article[] = [
+  {
+    name: "Tournevis",
+    price: 2.99,
+    qty: 120,
+  },
+  {
+    name: "Marteau",
+    price: 7.9,
+    qty: 45,
+  },
+  {
+    name: "Perceuse",
+    price: 25.99,
+    qty: 180,
+  },
+  {
+    name: "Pelle",
+    price: 15.0,
+    qty: 7,
+  },
+];
 
 app.use(cors());
 
 app.use((req, res, next) => {
   console.log("url:", req.url);
   next();
+});
+
+app.get("/api/articles", (req, res) => {
+  res.json(articles);
 });
 
 // app.get("/", (req, res) => {
